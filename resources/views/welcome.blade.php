d9199c17c2f3f6d356bbfcb5cba5e099d9940db6
timur518 <tkhalyapov@gmail.com>
2026-09-24 16:42:58 +0300

    правки

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=sofia-sans:400,500,600,700,800&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta name="mailru-domain" content="m7BvodbH4AQGo7RI" />
+
 </head>
 <body class="font-sans antialiased">
 
